393c290b30afc691de2c285f754c1f77d805c25e
UnrelSlayer <144865554+UnrelSlayer@users.noreply.github.com>
2024-10-05 19:45:39 -0300

    Add files via upload
    ciertas cosas que hice en c++

---
+++ c++ desde cero/lectura_entrada_datos.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+int N1 = 0 , N2 = 0 ;
+
+
+cout << "ingresa el primer numero:", cin >> N1;
+cout << "ingresa al segundo numero:", cin >> N2;
+int suma = N1 + N2;
+cout << suma;
+
+
+
+    return 0;
+}
---
+++ c++ desde cero/tipos_de_datos.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+
+int entero = 15;
+float decimal = 15.85;
+double mayor = 16.3431;
+char caracter = 'a';
+cout << entero << endl;
+cout << decimal << endl;
+cout << mayor << endl;
+cout << caracter << endl;
+system("pause");
+
+
+
+
+    return 0;
+}
